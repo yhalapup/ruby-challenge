7292206b41733a96f5500abf364403f70fe53480
Yurii <yurii.halapup@gmail.com>
2024-04-23 23:33:05 +0300

    Update environment variable REDIS_URL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.cache_store = :redis_cache_store, {url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }}
+  config.cache_store = :redis_cache_store, {url:ENV.fetch('REDIS_URL')}
 
 
   # Do not eager load code on boot.
