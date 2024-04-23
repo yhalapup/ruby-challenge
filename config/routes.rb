3c9aef3cd0513c764a5c56531608250b83b2a253
Yurii <yurii.halapup@gmail.com>
2024-04-23 23:31:06 +0300

    mount ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'user#index'
 
   resources :user
-  
+
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
