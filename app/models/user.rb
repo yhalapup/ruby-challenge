6ac91e5f1be9c4120229041123df614fbe28244d
Yurii <yurii.halapup@gmail.com>
2024-04-23 23:33:30 +0300

    Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :email, allow_blank: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
 end
